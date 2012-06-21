00ce731c6f15cd0647270b4efefe7375a96c888d
Andre R. Erler <aerler@atmosp.physics.utoronto.ca>
2012-06-20 23:05:09 -0400

    * first shot at execution script for WRF

--- Scripts/exec_WRF.sh
+++ Scripts/exec_WRF.sh
@@ -4,101 +4,58 @@
 
 # variable defined in driver script: 
 # $TASKS, $THREADS, $HYBRIDRUN, $INIDIR, $WORKDIR
+# for real.exe
+# $RUNREAL, $METDATA, $RAMIN, $RAMOUT
+# for WRF
+# $RUNWRF, $RAD, $LSM
 
-# RAM disk
-RAMDISK=/dev/shm/aerler/ # also set in Python script
-RAMDATA=$RAMDISK/data/ # data folder used by Python script
-# pyWPS.py
-if [ ! $RUNPYWPS ]; then RUNPYWPS=1; fi # whether to run runWPS.py
-METDATA=$WORKDIR/data/ # also set in Python script
-PYWPSLOG=$WORKDIR/pyWPS/ # log folder for Python script
-ALTSRC=$INIDIR/metgrid/  # alternate data source (if runWPS.py is not run)
 # real.exe
-if [ ! $RUNREAL ]; then RUNREAL=1; fi # whether to run real.exe
-if [ ! $REALRAM ]; then REALRAM=1; fi # run real.exe in RAM or on disk
-WRFINPUT=$WORKDIR/wrf_input/ # output folder for WRF input data
+RAMDATA=/dev/shm/aerler/data/ # RAM disk data folder
+if [ ! $RUNREAL ]; then RUNREAL=0; fi # whether to run real.exe
+if [ ! $RAMIN ]; then RAMIN=1; fi # process from hard disk
+if [ ! $RAMOUT ]; then RAMOUT=0; fi # write to hard disk
+METDATA=$INIDIR/metgrid/ # input files for real.exe
 REALLOG=$WORKDIR/real/ # log folder for real.exe
-# resolve working directory for real.exe
-if [ ! $REALDIR ] || [ $REALDIR == HD ]
-	then REALDIR=$WRFINPUT # write data directly to hard disk
-elif [ $REALDIR == RAM ] || [ $REALDIR == ram ]
-	then REALDIR=$RAMDATA # write data to RAM and copy to HD later
-fi
+# WRF
+if [ ! $RUNWRF ]; then RUNWRF=1; fi # whether to run WRF
+if [ ! $RAD ]; then $RAD='CAM'; fi # folder for WRF input data
+if [ ! $LSM ]; then LSM='Noah'; fi # output folder for WRF
 
-# remove existing temporary folder (ramdisk)
-rm -rf $RAMDISK
-# prepare working directories
-mkdir -p $RAMDISK # create ramdisk directory
-mkdir -p $RAMDATA # create data folder on ramdisk
-
-
-## run WPS driver script: pyWPS.py
-
-if [[ $RUNPYWPS == 1 ]]
-  then
-
-# launch feedback
-echo
-echo ' >>> Running WPS <<< '
-echo
-
-# specific environment for pyWPS.py
-# ´mkdir $METDATA´ is actually done by Python script
-cd $INIDIR 
-# copy links to source data (or create links)
-cp -P atm lnd ice pyWPS.py eta2p.ncl unccsm.exe metgrid.exe $WORKDIR
-cp -r meta/ $WORKDIR
-cp -P geo_em.d??.nc $WORKDIR # copy or link to geogrid files
-cp namelist.wps $WORKDIR # configuration file
-
-# run and time main pre-processing script (Python)
-cd $WORKDIR # using current working directory
-time -p python pyWPS.py
-wait
-
-# copy log files to disk
-rm $RAMDISK/tmp/*.nc $RAMDISK/tmp/meta/*.nc # remove data files
-cp -r $RAMDISK/tmp/ $PYWPSLOG # copy entire folder and rename
-
-# finish
-echo
-echo ' >>> WPS finished <<< '
-echo
-
-# if not running Python script, get data from disk
-else
-	echo
-	echo ' Copying source data to ramdisk.'
-	echo
-	cp $ALTSRC/*.nc $RAMDATA # copy alternate data to ramdisk
-
-fi # if RUNPYWPS
-
-
-echo 
-echo '   ***   ***   '
-echo
 
 ## run WRF pre-processor: real.exe
 
 if [[ $RUNREAL == 1 ]]
   then
-
-# launch feedback
 echo
 echo ' >>> Running real.exe <<< '
 echo
 
-# specific environment for real.exe
-mkdir -p $WRFINPUT # make data destination folder
-# create symbolic links to data on ramdisk
-# N.B.: This is not necessary since the input path can be changed in the namelist
-#cd $WRFINPUT
-#time -p for METFILE in $RAMDATA/met_em.*.nc; do ln -s $METFILE; done
+# if working on RAM disk get data from hard disk
+echo
+if [[ $RAMIN == 1 ]]
+  then
+    # prepare RAM disk
+    rm -rf $RAMDISK # remove existing temporary folder (ramdisk)
+    mkdir -p $RAMDATA # create data folder on ramdisk
+    echo ' Copying metgrid data to ramdisk:'
+    cp $METDATA/met_em.*.nc $RAMDATA # copy metgrid data to ramdisk
+else
+    echo ' Using metgrid data from:'
+fi
+echo $METDATA
+echo
+# resolve working directory for real.exe
+if [[ $RAMOUT == 1 ]]
+  then 
+	REALDIR=$RAMDATA # write data to RAM and copy to HD later
+else
+    REALDIR=$WORKDIR # write data directly to hard disk
+fi
+
 
 # copy namelist and link to real.exe into working director
 cp -P $INIDIR/real.exe $REALDIR # link to executable real.exe
-cp $INIDIR/namelist.input $REALDIR # copy namelists
+cp $INIDIR/namelist.input $REALDIR # copy namelist
 
 ## run and time hybrid (mpi/openmp) job
 cd $REALDIR # so that output is written here
@@ -108,31 +65,81 @@ echo $HYBRIDRUN ./real.exe
 echo
 echo Writing output to $REALDIR
 echo
+# launch
 time -p $HYBRIDRUN ./real.exe
 wait
 
-# clean-up output folder on hard disk
-#rm met_em.*.nc real.exe # remove met file links
-# save log files and meta data
+## finish / clean-up
 mkdir $REALLOG # make folder for log files
 cd $REALDIR
+# save log files and meta data
 mv rsl.*.???? namelist.input namelist.output real.exe $REALLOG
 # copy/move date to output directory (hard disk) if necessary
-if [[ ! $REALDIR == $WRFINPUT ]]
+if [[ ! $REALDIR == $WORKDIR ]]
   then 
-	echo Copying data to $WRFINPUT
-	time -p mv wrf* $WRFINPUT
-  fi
-
+	echo ' Copying WRF input data to:'
+    echo $WORKDIR
+	time -p mv wrf*_d?? $WORKDIR
+fi
+# clean-up RAM disk
+if [[ $RAMIN == 1 ]] || [[ $RAMOUT == 1 ]]
+  then rm -rf $RAMDATA 
+fi
 # finish
 echo
 echo ' >>> real.exe finished <<< '
 echo
+echo
+echo '   ***   ***   ***   '
+echo
 
+## otherwise copy input data from initial directory
+else
+    cp -P $INIDIR/wrf*_d?? $WORKDIR
 fi # if RUNREAL
 
 
-## finish / clean-up
+## run WRF pre-processor: real.exe
 
-# delete temporary data
-rm -r $RAMDISK
+if [[ $RUNWRF == 1 ]]
+  then
+echo
+echo ' >>> Running WRF <<< '
+echo
+
+## link/copy relevant input files
+cd $INIDIR # folder containing input files
+# essentials
+cp -P namelist.input wrf.exe $WORKDIR
+# radiation scheme
+if [[ $RAD == 'CAM' ]]; then 
+    cp -P CAM* ozone* $WORKDIR
+elif [[ $RAD == 'RRTMG' ]]; then 
+    cp -P RRTMG_* $WORKDIR
+elif [[ $RAD == 'RRTM' ]]; then 
+    cp -P RRTM_* $WORKDIR
+else
+    echo 'WARNING: no radiation scheme selected!'
+fi
+# land-surface scheme
+if [[ $LSM == 'Noah' ]] || [[ $LSM == 'RUC' ]]; then 
+    cp -P SOILPARM.TBL VEGPARM.TBL GENPARM.TBL LANDUSE.TBL $WORKDIR
+elif [[ $RAD == 'Diff' ]]; then 
+    cp -P LANDUSE.TBL $WORKDIR
+else
+    echo 'WARNING: no land-surface model selected!'
+fi
+
+## run and time hybrid (mpi/openmp) job
+cd $WORKDIR
+echo
+echo OMP_NUM_THREADS=$THREADS
+echo $HYBRIDRUN ./wrf.exe
+echo
+echo Writing output to $REALDIR
+echo
+# launch
+time -p $HYBRIDRUN ./wrf.exe
+wait
+
+fi # if RUNWRF
