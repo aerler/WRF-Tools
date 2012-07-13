4b8d36c4225312257cbbe26db366dfe4661ac89a
Andre R. Erler <aerler@atmosp.physics.utoronto.ca>
2012-07-13 15:56:22 -0400

    * wrote script to initiate cycling on GPC  * adapted WPS run-script for cycling on GPC  * some small stuff

---
+++ Scripts/GPC/run_cycle_pbs.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# script to set up a cycling WPS/WRF run: reads first entry in stepfile and 
+# starts/submits first WPS and WRF runs, the latter dependent on the former
+# created 28/06/2012 by Andre R. Erler, GPL v3
+
+# settings
+set -e # abort if anything goes wrong
+export STEPFILE='stepfile.hourly' # file in $INIDIR
+export INIDIR="${PWD}" # current directory
+
+# launch feedback
+echo
+echo "   ***   Starting Cycle  ***   "
+echo
+echo "   Stepfile: ${STEPFILE}"
+echo "   Root Dir: ${INIDIR}"
+echo 
+
+# clear some folders
+export METDATA="${INIDIR}/metgrid/"
+export WRFOUT="${INIDIR}/wrfout/"
+echo "   Clearing Output Folders:"
+echo "${METDATA}"
+echo "${WRFOUT}"
+rm -rf "${METDATA}" "${WRFOUT}" 
+
+# read first entry in stepfile 
+NEXTSTEP=$(python cycling.py)
+#export NEXTSTEP
+echo
+echo "   First Step: ${NEXTSTEP}"
+echo
+
+# prepare first working directory
+# set restart to False for first step
+sed -i '/restart\s/ s/restart\s*=\s*\.true\..*$/restart = .false.,/' \
+ "${INIDIR}/${NEXTSTEP}/namelist.input"  
+# and make sure the rest is on restart
+sed -i '/restart\s/ s/restart\s*=\s*\.false\..*$/restart = .true.,/' \
+ "${INIDIR}/namelist.input"
+echo 
+echo "  Setting restart option in namelist."
+echo
+
+# submit first WPS instance
+qsub ./run_cycling_WPS.pbs -v NEXTSTEP=$NEXTSTEP  
+
+# submit first WRF instance
