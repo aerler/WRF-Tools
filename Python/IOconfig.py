77c7ff03a8d3fe0134c81ee728bad03de1008124
Andre R. Erler <aerler@atmosp.physics.utoronto.ca>
2012-09-20 18:41:47 -0400

    * implemented core of I/O stream configuration program (automatic registry editor) TODO: more testing, handle multiple streams

--- Python/IOconfig.py
+++ Python/IOconfig.py
@@ -12,6 +12,7 @@ import fileinput # read the I/O config and write the registry file
 import sys # write to stdout (used with fileinput)
 import warnings # alert the user of syntax errors
 import shutil # copy the original
+import re # to parse I/O strings
 
 ## settings 
 # full path to I/O config file
@@ -22,6 +23,59 @@ registryfile = '/home/me/Models/WRF Tools/misc/registry/test/Registry.EM_COMMON'
 oldregistry  = '/home/me/Models/WRF/WRFV3/Registry/Registry.EM_COMMON.original'
 # more settings 
 recurse = False # also apply changes to 'included' registry files (currently not implemented)
+
+## actual editing functions
+
+# globally used regular expressions
+dusffct = re.compile(r'[udsf]=\([^()]*\)') # match nesting operations with function specification
+dusf = re.compile(r'[udsf]') # match nesting operations without function specification
+
+# fct for pre-conditioning an I/O stream
+def processIOstream(iostr, addrm, iotype, ioid):
+  # regular expression defining the stream we are interested in
+  irh = re.compile(iotype+r'[0-9{}]*') # match the required I/O stream
+  # remove irrelevant fields
+  excess = str().join(dusffct.findall(iostr)) # what we don't care about, but need to keep 
+  iostr = str().join(dusffct.split(iostr)) # what we are actually interested in
+  excess = str().join(dusf.findall(iostr)) + excess
+  iostr = str().join(dusf.split(iostr))
+  ## process actual I/O streams (and keep order)
+  # N.B.: I don't know what happens if multiple instances occur
+  iostrs = irh.split(iostr) # list of other I/O streams
+  assert len(iostrs) == 2 
+  iostr = irh.findall(iostr) # the I/O stream we are operating on 
+  assert len(iostr) == 1 
+  iostr = iostr[0]
+  # string representation of I/O stream
+  if ioid > 9:
+    assert ioid < 100
+    ioidstr = '{%2i}'%ioid
+    ddstr = '' # see 'else' below
+  else:
+    ioidstr = '%1i'%ioid
+    # also need to protect double-digit numbers from change
+    dd = re.compile(r'{\d\d}')
+    ddstr = str().join(dd.findall(iostr))
+    iostr = str().join(dd.split(iostr))
+  ## add stream
+  if addrm:
+    # add implicit zero
+    if iostr == iotype:
+      iostr = iostr + '0' 
+    # add new stream is not already present
+    if ioidstr not in iostr:
+      iostr = iostr + ioidstr
+  ## remove stream
+  else:
+    # remove existing stream (if actually present)
+    if ioidstr in iostr:
+      iostr = iostr.replace(ioidstr,'')
+    # remove stream to avoid implicit zero
+    if iostr == iotype:
+      iostr = '' 
+  newiostr = str().join([iostrs[0], iostr, ddstr, iostrs[1], excess])
+  return newiostr
+
 
 # start execution
 if __name__ == '__main__':
@@ -43,7 +97,7 @@ if __name__ == '__main__':
       feedback = ' the following variables ' # tell the user what we are doing
       err = 0
       # split into tokens
-      tokens = [token.strip() for token in line.split(':', 3)] # colon delimiter, max 3 splits, remove white spaces 
+      tokens = [token.strip() for token in line.lower().split(':', 3)] # colon delimiter, max 3 splits, remove white spaces 
       # operation: addrm
       if tokens[0] == '+': 
         addrm = True
@@ -78,7 +132,7 @@ if __name__ == '__main__':
         warnings.warn('WARNING: No legal stream ID: '+tokens[3]+' \n'+line)
       feedback = feedback + ' stream # ' + tokens[2]
       # list of variables
-      variables = tokens[3].split(',')
+      variables = tokens[3].lower().split(',')
       feedback = feedback + ':\n ' + variables[0]
       for variable in variables[1:]:
         feedback += ', ' + variable
@@ -112,7 +166,7 @@ if __name__ == '__main__':
     if oldline:
       line = oldline + line # oldline will be empty is line is self-contained
     # check for more line continuation 
-    if line[-1] == "\\": # need to escape backslash
+    if line[-1:] == '\\': # need to escape backslash
       oldline = line[:-1] # remove backslash
     # if the line is complete, process it  
     else:
@@ -122,10 +176,27 @@ if __name__ == '__main__':
       # check for affected variables
       else:
         tokens = line.split(None, 8) # white space delimiter, max 8 splits (to keep names and units intact)
-        newline = tokens[0]
-        for token in tokens[1:]:
-          newline = newline + '    ' + token
-        sys.stdout.write(newline+'\n')
+        # N.B.: by limiting the splits to 8, we don't have to deal with the description and unit sections,
+        #       which contain white spaces, and would not be easily separable
+        ## identify affected variables
+        # standard format has 10 entries, but we only split 8 times
+        if len(tokens) == 9:
+          # loop over variable list
+          for var in variables:
+            # search for variable name in 3rd field (all lower case)
+            if tokens[2].lower() == var.lower():
+              oldiostr = tokens[7].lower()
+              ## here comes the editing of the actual I/O string
+              newiostr = processIOstream(oldiostr, addrm, iotype, ioid)              
+              tokens[7] = newiostr
+          # write modified line into file
+          newline = tokens[0]
+          for token in tokens[1:]:
+            newline = newline + '    ' + token
+          sys.stdout.write(newline+'\n')
+        # if the line doesn't have 9 tokens, it is something else...
+        else:
+          sys.stdout.write(line+'\n')
       oldline = '' # lines must not grow indefinitely
   # close WRF registry file
   fileinput.close()
