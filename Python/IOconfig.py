697d762e1e8b09302e882c9bab9d4c15a966032d
Andre R. Erler <aerler@atmosp.physics.utoronto.ca>
2012-09-21 18:33:18 -0400

    * implemented recursively adding new registry files through include statements

--- Python/IOconfig.py
+++ Python/IOconfig.py
@@ -2,9 +2,9 @@
 Created on 2012-09-19
 
 A Python module to read WRF I/O-config entries (ascii text files) and write the changes 
-to I/O streams into the WRF Registry  
+to I/O streams into WRF Registry files
 
-@author: Andre R. Erler
+@author: Andre R. Erler, GPL v3
 '''
 
 # imports
@@ -18,14 +18,17 @@ import re # to parse I/O strings
 # full path to I/O config file
 ioconfigfile = '/home/me/Models/WRF Tools/misc/registry/test/ioconfig.test' 
 # full path to WRF registry file (destination)
-registryfile = '/home/me/Models/WRF Tools/misc/registry/test/Registry.EM_COMMON'
+newregfolder = '/home/me/Models/WRF Tools/misc/registry/test/'
+newregfiles = ['Registry.EM']
 # full path to WRF registry file (source) 
-oldregistry  = '/home/me/Models/WRF/WRFV3/Registry/Registry.EM_COMMON.original'
-# more settings 
-table = True # search table field instead of variable field (e.g. you can remove all state variables)
-rmall = True # allow keyword 'all' as stream ID to remove all  
-recurse = False # also apply changes to 'included' registry files (currently not implemented)
-debug = True # print all changes to I/O string
+oldregfolder = '/home/me/Models/WRF/WRFV3/Registry/original/'
+oldregfiles = ['/Registry.EM'] or newregfiles # ['/Registry.EM_COMMON.original']
+# more settings
+lstate = True # only operate on state-variables (faster) 
+ltable = False # search table field instead of variable field (e.g. you can remove all state variables)
+lrmall = True # allow keyword 'all' as stream ID to remove all  
+lrecurse = True # also apply changes to 'included' registry files (currently not implemented)
+ldebug = True # print all changes to I/O string
 
 ## actual editing functions
 
@@ -88,7 +91,7 @@ def processIOstream(oldiostr, addrm, iotype, ioid):
   ## remove stream
   else:
     # remove all streams
-    if rmall and (ioidstr == 'all'):
+    if lrmall and (ioidstr == 'all'):
       iostr = ''
     # remove an existing stream (if actually present)
     elif ioidstr in iostr:
@@ -119,7 +122,7 @@ if __name__ == '__main__':
     if not '#' in line:
       entryno += 1     
       tmpdict = dict() # dictionary of parameters 
-      if debug:
+      if ldebug:
         print('') 
         print('Reading I/O config Entry # '+str(entryno))
         feedback = ' the following variables ' # tell the user what we are doing
@@ -129,10 +132,10 @@ if __name__ == '__main__':
       # operation: addrm
       if tokens[0] == '+': 
         addrm = True
-        if debug: feedback = '  Adding' + feedback + 'to '  
+        if ldebug: feedback = '  Adding' + feedback + 'to '  
       elif tokens[0] == '-': 
         addrm = False
-        if debug: feedback = '  Removing' + feedback + 'from '
+        if ldebug: feedback = '  Removing' + feedback + 'from '
       else: 
         addrm = None
         err += 1
@@ -142,13 +145,13 @@ if __name__ == '__main__':
       # stream type: iotype
       if tokens[1] == 'i': 
         iotype = 'i'
-        if debug: iotypetmp = 'input' # used below 
+        if ldebug: iotypetmp = 'input' # used below 
       elif tokens[1] == 'r': 
         iotype = 'r'
-        if debug: iotypetmp = 'restart' # used below 
+        if ldebug: iotypetmp = 'restart' # used below 
       elif tokens[1] == 'h': 
         iotype = 'h'
-        if debug: iotypetmp = 'history' # used below
+        if ldebug: iotypetmp = 'history' # used below
       else:
         iotype = None 
         err += 1
@@ -156,9 +159,9 @@ if __name__ == '__main__':
       # save in dictionary
       tmpdict['iotype'] = iotype
       # stream id/number: ioid
-      if rmall and (not addrm) and (tokens[2] == 'all'):
+      if lrmall and (not addrm) and (tokens[2] == 'all'):
         ioid = 'all'
-        if debug: feedback = '  Removing the following variables from all ' + iotypetmp + ' streams'      
+        if ldebug: feedback = '  Removing the following variables from all ' + iotypetmp + ' streams'      
       else:
         try:
           ioid = int(tokens[2])        
@@ -166,12 +169,12 @@ if __name__ == '__main__':
           ioid = None
           err += 1
           warnings.warn('WARNING: No legal stream ID: '+tokens[3]+' \n'+line)
-        if debug: feedback = feedback + iotypetmp + ' stream # ' + tokens[2]
+        if ldebug: feedback = feedback + iotypetmp + ' stream # ' + tokens[2]
       # save in dictionary
       tmpdict['ioid'] = ioid
       # list of variables
       variables = tokens[3].lower().split(',')
-      if debug: 
+      if ldebug: 
         feedback = feedback + ':\n   ' + variables[0]
         for variable in variables[1:]:
           feedback += ', ' + variable
@@ -179,7 +182,7 @@ if __name__ == '__main__':
       tmpdict['variables'] = variables
       # print feedback (tell user what we are doing)
       if err == 0:
-        if debug: print(feedback)
+        if ldebug: print(feedback)
         entrylist.append(tmpdict)
       # or exit, if errors occurred
       else:
@@ -187,9 +190,14 @@ if __name__ == '__main__':
         
   # close I/O config file
   fileinput.close()
+  
+  print('')
+  print('')
+  print('      ***   ***   ***   ***   ***   ***   ***   ***   ')
+  print('')
 
 #      # debugging output
-#      if debug:
+#      if ldebug:
 #        print
 #        print 'Debugging Info:'
 #        print addrm
@@ -197,89 +205,101 @@ if __name__ == '__main__':
 #        print ioid
 #        print variables
 #        print
-            
-      
-  ## rewrite WRF registry
-  # copy the original to the new destination (if given) 
-  if oldregistry:
-    shutil.copy(oldregistry, registryfile)
-  
-  ## loop over I/O config entries
-  entryno = 0 # list of changes per entry  
-  for entry in entrylist:
-    entryno += 1
-    # open with fileinput for editing
-    registry = fileinput.FileInput([registryfile], inplace=True) # apparently AIX doesn't like "mode='r'"
-    # regurgitate parameter values
-    addrm = entry['addrm'] # operation (True: add, False: remove)
-    iotype = entry['iotype'] # I/O stream type (i, r, h)
-    ioid = entry['ioid'] # I/O stream ID (0-9, {10}-{23})
-    variables = entry['variables'] # List of variables affected by the operation
-    # save list of changes
-    if debug: changelog = []
-    # loop over lines
-    oldline = '' # used to reassemble line continuations
+                  
+  ## loop over and rewrite WRF registry files
+  baseno = len(newregfiles) # original number of files
+  fileno = 0 # file counter
+  while fileno < len(newregfiles):
+    # grab files to work on        
+    newregfile = newregfiles[fileno]
+    oldregfile = oldregfiles[fileno]
+    fileno += 1 # move up counter
+    # announce files
+    print('')
+    print('   ***   Processing Registry file: '+newregfile+'   ***   ')
+    if fileno > baseno: print('            (file was automatically included)')
+    print('')
+    # copy the original to the new destination (if given) 
+    if oldregfolder:
+      shutil.copy(oldregfolder+oldregfile, newregfolder+newregfile)
     
-    ## loop over lines
-    for line in registry:
-      line = line.strip() # remove leading/trailing spaces
-      # reassemble line continuations
-      if oldline:
-        line = oldline + line # oldline will be empty is line is self-contained
-      # check for more line continuation 
-      if line[-1:] == '\\': # need to escape backslash
-        oldline = line[:-1] # remove backslash
-      # if the line is complete, process it  
-      else:
-        # skip comments and empty lines
-        if (line == '') or (line[0] == '#'):
-          sys.stdout.write(line+'\n')
-        # check for affected variables
+    ## loop over I/O config entries
+    entryno = 0 # list of changes per entry  
+    for entry in entrylist:
+      entryno += 1
+      # open with fileinput for editing
+      registry = fileinput.FileInput([newregfolder+newregfile], inplace=True) # apparently AIX doesn't like "mode='r'"
+      # regurgitate parameter values
+      addrm = entry['addrm'] # operation (True: add, False: remove)
+      iotype = entry['iotype'] # I/O stream type (i, r, h)
+      ioid = entry['ioid'] # I/O stream ID (0-9, {10}-{23})
+      variables = entry['variables'] # List of variables affected by the operation
+      # save list of changes
+      if ldebug: changelog = []
+      # loop over lines
+      oldline = '' # used to reassemble line continuations
+      
+      ## loop over lines
+      for line in registry:
+        line = line.strip() # remove leading/trailing spaces
+        # reassemble line continuations
+        if oldline:
+          line = oldline + line # oldline will be empty is line is self-contained
+        # check for more line continuation 
+        if line[-1:] == '\\': # need to escape backslash
+          oldline = line[:-1] # remove backslash
+        # if the line is complete, process it  
         else:
-          tokens = line.split(None, 8) # white space delimiter, max 8 splits (to keep names and units intact)
-          # N.B.: by limiting the splits to 8, we don't have to deal with the description and unit sections,
-          #       which contain white spaces, and would not be easily separable          
-          ## identify affected variables
-          # standard format has 10 entries, but we only split 8 times
-          if len(tokens) == 9:
-            # decide which column to go by
-            if table: mode = 0 # process all of a type
-            else: mode = 2 # process by variable name
-            # loop over variable list
-            for var in variables:
-              # search for variable name in 3rd field (all lower case)
-              if tokens[mode].lower() == var.lower():
-                oldiostr = tokens[7].lower()
-                ## here comes the editing of the actual I/O string
-                print var
-                print oldiostr
-                newiostr = processIOstream(oldiostr, addrm, iotype, ioid)
-                # save modifications in string and add to change-log              
-                if debug: 
-                  if oldiostr != newiostr:
-                    changelog.append(tokens[2]+':  '+oldiostr+'  >>>  '+newiostr)                
-                tokens[7] = newiostr
-            # write modified line into file
-            newline = tokens[0]
-            for token in tokens[1:]:
-              newline = newline + '    ' + token
-            sys.stdout.write(newline+'\n')
-          # if the line doesn't have 9 tokens, it is something else...
-          else:
+          # skip comments and empty lines
+          if (line == '') or (line[0] == '#'):
             sys.stdout.write(line+'\n')
-        oldline = '' # lines must not grow indefinitely
-
-    # close WRF registry file
-    fileinput.close()
+          # check for affected variables
+          else:
+            tokens = line.split(None, 8) # white space delimiter, max 8 splits (to keep names and units intact)
+            # N.B.: by limiting the splits to 8, we don't have to deal with the description and unit sections,
+            #       which contain white spaces, and would not be easily separable          
+            ## identify affected variables
+            # standard format has 10 entries, but we only split 8 times
+            if (len(tokens) == 9) and ((not lstate) or (tokens[0].lower() == 'state')):
+              # decide which column to go by
+              if ltable: mode = 0 # process all of a type
+              else: mode = 2 # process by variable name
+              # loop over variable list
+              for var in variables:
+                # search for variable name in 3rd field (all lower case)
+                if tokens[mode].lower() == var.lower():
+                  oldiostr = tokens[7].lower()
+                  ## here comes the editing of the actual I/O string
+                  newiostr = processIOstream(oldiostr, addrm, iotype, ioid)
+                  # save modifications in string and add to change-log              
+                  if ldebug: 
+                    if oldiostr != newiostr:
+                      changelog.append(tokens[2]+':  '+oldiostr+'  >>>  '+newiostr) # use actual variable name                 
+                  tokens[7] = newiostr
+              # write modified line into file
+              newline = tokens[0]
+              for token in tokens[1:]:
+                newline = newline + '    ' + token
+              sys.stdout.write(newline+'\n')
+            elif lrecurse and (tokens[0].lower() == 'include'):
+              newregfiles.append(tokens[1])
+              oldregfiles.append(tokens[1])
+            # if the line doesn't have 9 tokens, it is something else...
+            else:
+              sys.stdout.write(line+'\n')
+          oldline = '' # lines must not grow indefinitely
   
-    # print debugging info / change-log
-    if debug:
-      print('')
-      print('Processed I/O config Entry # '+str(entryno))
-      print('  Log of changes:')
-      if len(changelog) == 0:
-        print('   no changes')
-      else:
-        for line in changelog:
-          print('   '+line)
-  
+      # close WRF registry file
+      fileinput.close()
+    
+      # print debugging info / change-log
+      if ldebug:
+        print('')
+        print('Processed I/O config Entry # '+str(entryno))
+        print('  Log of changes:')
+        if len(changelog) == 0:
+          print('   no changes')
+        else:
+          for line in changelog:
+            print('   '+line)
+    
