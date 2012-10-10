b12542947275c58e844fac6965c61dfb07156dbb
Andre R. Erler <aerler@atmosp.physics.utoronto.ca>
2012-10-10 14:58:23 -0400

    * renamed eta2p.ncl to unccsm.ncl  * made new script folder for GPC-lm (largemem)  * revised handlign of machine dependency in setup script

--- Scripts/setupIniDir.sh
+++ Scripts/setupIniDir.sh
@@ -5,93 +5,83 @@
 # environment variables: $MODEL_ROOT, $WPSSRC, $WRFSRC
 
 ## TODO:
-# * reconsider neccessity of ocean grid definition files
-# * config file needs a standard name
-# * include date range in stepfile filename
+# * generate namelists in setup script
+# * change name of experiment
 # * handle GHG scenario in setup script, not in run-script
 # * consider putting exec* scripts into PATH
-# * differentiate between GPC-batch and GPC-largemem 
 
 ## load configuration 
-source config.setup
-# run folder
-RUNDIR="${PWD}"
+source xconfig.sh
+# make run folder
+# RUNDIR="${PWD}" in xconfig.sh 
 mkdir -p "${RUNDIR}"
 
 # link meta data
 mkdir -p "${RUNDIR}/meta"
 cd "${RUNDIR}/meta"
-ln -sf "${WRFTOOLS}/misc/data/${POPMAP}" ${POPMAP}  
-ln -sf "${WRFTOOLS}/misc/data/${GEOGRID}" GEOGRID.TBL
-ln -sf "${WRFTOOLS}/misc/data/${METGRID}" METGRID.TBL
-ln -sf "${WRFTOOLS}/misc/data/${UNCCSM}" namelist.data
-ln -sf "${WRFTOOLS}/misc/data/${PYWPS}" namelist.py
-ln -sf "${WRFTOOLS}/misc/data/${NCL}" setup.ncl
+ln -sf "${WRFTOOLS}/misc/data/${POPMAP}"  
+ln -sf "${WRFTOOLS}/misc/data/${GEOGRID}" 'GEOGRID.TBL'
+ln -sf "${WRFTOOLS}/misc/data/${METGRID}" 'METGRID.TBL'
+ln -sf "${WRFTOOLS}/misc/data/${NCL}" 'setup.ncl'
 # link boundary data
 cd "${RUNDIR}"
-ln -sf "${DATADIR}/atm/hist/" "atm" # atmosphere
-ln -sf "${DATADIR}/lnd/hist/" "lnd" # land surface
-ln -sf "${DATADIR}/ice/hist/" "ice" # sea ice
+ln -sf "${DATADIR}/atm/hist/" 'atm' # atmosphere
+ln -sf "${DATADIR}/lnd/hist/" 'lnd' # land surface
+ln -sf "${DATADIR}/ice/hist/" 'ice' # sea ice
 
 ## link in WPS stuff
-# WPS scripts and executables
+# queue system
+if [[ "${WPSSYS}" == "GPC"* ]]; then 
+	WPSQ='pbs' # GPC standard and largemem nodes
+elif [[ "${WPSSYS}" == "P7" ]]; then
+	WPSQ='ll'
+fi
+# WPS scripts
 ln -sf "${WRFTOOLS}/Scripts/prepWorkDir.sh"
 ln -sf "${WRFTOOLS}/Scripts/execWPS.sh"
 ln -sf "${WRFTOOLS}/Python/pyWPS.py"
 ln -sf "${WRFTOOLS}/NCL/eta2p.ncl"
-# WPS GPC-specific stuff (for largemem-nodes)
-if [[ "$WPSSYS" == "GPC" ]]; then
-	ln -sf "${WRFTOOLS}/Scripts/GPC/setupGPC.sh"
-	ln -sf "${WRFTOOLS}/bin/GPC-lm/unccsm.exe"
-	ln -sf "${WPSSRC}/GPC-MPI/O3xHost/geogrid.exe"
-	ln -sf "${WPSSRC}/GPC-MPI/O3xSSSE3/metgrid.exe"
-	ln -sf "${WRFSRC}/GPC-MPI-Clim/O3xSSSE3/real.exe"
-	if [[ -n "${CYCLING}" ]]; then
-		cp "${WRFTOOLS}/Scripts/GPC/run_cycling_WPS.pbs" .
-	else
-		cp "${WRFTOOLS}/Scripts/GPC/run_test_WPS.pbs" .
-	fi		
-fi
-# Some modifications if WRF is running on TCS
-if [[ "$WRFSYS" == "TCS" ]]; then
-	ln -sf "${WPSSRC}/TCS-MPI/NoOptO3/geogrid.exe"
-fi
+# platform dependent stuff
+ln -sf "${WRFTOOLS}/bin/${WPSSYS}/unccsm.exe"
+ln -sf "${WRFTOOLS}/Scripts/${WPSSYS}/setup${WPSSYS}.sh"
+# if cycling
+if [[ -n "${CYCLING}" ]]; then
+	cp "${WRFTOOLS}/Scripts/${WPSSYS}/run_cycling_WPS.${WPSQ}" .
+else
+	cp "${WRFTOOLS}/Scripts/${WPSSYS}/run_test_WPS.${WPSQ}" .
+fi		
+# WPS/WRF executables
+ln -sf "${GEOEXE}"
+ln -sf "${METEXE}"
+ln -sf "${REALEXE}"
 
 ## link in WRF stuff
+# queue system
+if [[ "${WRFSYS}" == "GPC" ]]; then 
+	WRFQ='pbs' # GPC standard and largemem nodes
+elif [[ "${WRFSYS}" == "TCS" ]] || [[ "${WRFSYS}" == "P7" ]]; then
+	WRFQ='ll'
+fi
+# WRF scripts
 cd "${RUNDIR}"
 ln -sf "${WRFTOOLS}/Scripts/prepWorkDir.sh"
 ln -sf "${WRFTOOLS}/Scripts/execWRF.sh"
-ln -sf "${WRFTOOLS}/misc/tables" 'tables' # WRF default tables
-#ln -sf "${WRFTOOLS}/misc/tables/" # new tables includign Noah-MP stuff 
-# WRF on GPC
-if [[ "$WRFSYS" == "GPC" ]]; then
-	ln -sf "${WRFTOOLS}/Scripts/GPC/setupGPC.sh"
-	if [[ -n "${CYCLING}" ]]; then
-        ln -sf "${WRFTOOLS}/Scripts/GPC/run_cycle_pbs.sh"
-        ln -sf "${WRFTOOLS}/Python/cycling.py"
-        cp "${WRFTOOLS}/misc/namelists/stepfile.${CYCLING}" 'stepfile' 
-		cp "${WRFTOOLS}/Scripts/GPC/run_cycling_WRF.pbs" .
-	else
-		cp "${WRFTOOLS}/Scripts/GPC/run_test_WRF.pbs" .
-	fi
-	ln -sf "${WRFSRC}/GPC-MPI-Clim/O3xHost/wrf.exe"
+ln -sf "${WRFTOOLS}/misc/tables" # WRF default tables
+#ln -sf "${WRFTOOLS}/misc/tables-NoahMP" 'tables' # new tables including Noah-MP stuff 
+# if cycling
+ln -sf "${WRFTOOLS}/Scripts/${WRFSYS}/setup${WRFSYS}.sh"
+if [[ -n "${CYCLING}" ]]; then
+	ln -sf "${WRFTOOLS}/Scripts/${WRFSYS}/run_cycle_*.sh"
+	ln -sf "${WRFTOOLS}/Python/cycling.py"
+	cp "${WRFTOOLS}/misc/namelists/stepfile.${CYCLING}" 'stepfile' 
+	cp "${WRFTOOLS}/Scripts/${WRFSYS}/run_cycling_WRF.*" .
+else
+	cp "${WRFTOOLS}/Scripts/${WRFSYS}/run_test_WRF.*" .
 fi
-# WRF on TCS
-if [[ "$WRFSYS" == "TCS" ]]; then
-	ln -sf "${WRFTOOLS}/Scripts/TCS/setupTCS.sh"
-	cp "${WRFTOOLS}/Scripts/TCS/run_test_WRF.ll" .
-    if [[ -n "${CYCLING}" ]]; then
-        ln -sf "${WRFTOOLS}/Scripts/TCS/run_cycle_ll.sh"
-        ln -sf "${WRFTOOLS}/Python/cycling.py"
-        cp "${WRFTOOLS}/misc/namelists/stepfile.${CYCLING}" 'stepfile' 
-        cp "${WRFTOOLS}/Scripts/TCS/run_cycling_WRF.ll" .
-    else
-        cp "${WRFTOOLS}/Scripts/TCS/run_test_WRF.ll" .
-    fi
-	ln -sf "${WRFSRC}/TCS-MPI-Clim/O3q64/wrf.exe"
-fi
+# WRF executable
+ln -sf "${WRFEXE}"
 
 ## prompt user to create data links
 echo "Remainign tasks:"
-echo " * review meta data (meta/ and tables/) and edit namelists"
-echo " * adapt run scripts, if necessary" 
+echo " * review meta data and namelists"
+echo " * edit run scripts, if necessary" 
---
+++ Scripts/xconfig.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+## scenario definition section
+# GHG emission scenario
+GHG='A2' # CAMtr_volume_mixing_ratio.* file to be used
+# time period and cycling interval
+CYCLING="monthly" # stepfile to be used (leave empty if not cycling)
+
+## namelist definition section
+# list of namelist groups and used snippets
+# WRF
+TIME_CONTROL='cycling,fineio'
+DIAGS='hitop'
+PHYSICS='clim'
+DOMAINS='wc02'
+FDDA='spectral'
+DYNAMICS='default'
+BDY_CONTROL='clim'
+NAMELIST_QUILT=''
+# WPS
+SHARE='default'
+GEOGRID="${DOMAINS}"
+METGRID='pywps'
+
+## configure data sources
+RUNDIR="${PWD}"
+# source data definiton 
+NCL="setup.ncl.cesm" # CESM grid parameters (for sea-ice) for NCL
+DATADIR="/scratch/p/peltier/marcdo/archive/tb20trcn1x1/" 
+# other WPS configuration files
+POPMAP="map_gx1v6_to_fv0.9x1.25_aave_da_090309.nc" # ocean grid definition
+GEOGRID="GEOGRID.TBL.FLAKE"
+METGRID="METGRID.TBL.CESM"
+
+## system settings
+WRFTOOLS="${MODEL_ROOT}/WRF Tools/"
+# WPS executables
+WPSSYS="GPC" # general system
+GEOEXE="${WPSSRC}/GPC/Clim-fullIO/O3xHost/geogrid.exe"
+METEXE="${WPSSRC}/GPC/Clim-fullIO/O3xHost/metgrid.exe"
+REALEXE="${WRFSRC}/GPC/Clim-fineIO-03/O3xHost/real.exe"
+# WRF executable
+WRFSYS="GPC" # WRF
+WRFEXE="${WRFSRC}/GPC/Clim-fineIO-03/O3xHost/wrf.exe"
