183e05955586995fbd9cb9524976a35928d55998
Andre R. Erler <aerler@atmosp.physics.utoronto.ca>
2013-02-20 16:58:49 -0500

    * finished estimate of queue time TODO: pass value back to caller

--- Python/estimateQueue.py
+++ Python/estimateQueue.py
@@ -9,6 +9,8 @@ Short script to estimate expected queue wait times.
 # imports
 import socket # recognizing host 
 import subprocess
+import warnings
+import numpy
 # maybe imports ...
 import os # directory operations
 import fileinput # reading and writing config files
@@ -20,9 +22,15 @@ import calendar # to locate leap years
 ##  determine if we are on SciNet or my local machine
 hostname = socket.gethostname()
 if (hostname=='komputer') or (hostname=='erlkoenig'):
+  # this is for test purpose only
+  nodes = 2 # number of nodes
+  ppn = 16 # processes per node 
   showq = 'cat queue-test.txt' # test dummy
 elif ('gpc' in hostname):
-  showq = 'showq -w class=largemem'
+  # we need to know something about the queue system...
+  nodes = 2 # number of nodes
+  ppn = 16 # processes per node 
+  showq = 'showq -w class=largemem' # queue query command
 
 if __name__ == '__main__':
   
@@ -34,15 +42,49 @@ if __name__ == '__main__':
   # parse output
   for line in cmd.stdout:
     lrun = lidl = False # reset
-    if "Running" in line: lrun = True 
-    elif "Idle" in line: lidl = True
+    linesplit = line.split()
+    if len(linesplit) == 9: 
+      if "Running" == linesplit[2]: lrun = True 
+      elif "Idle" == linesplit[2]: lidl = True
     # process time
-    if lrun or lidl: 
-      time = line.split()[4]
-      # convert time format... 
-      if lrun: print 'Running: %s'%time  
-      elif lidl: print 'Idle: %s'%time
-      
+    if lrun or lidl:
+      np =  int(linesplit[3])
+      assert np == 16 or np == 32, warnings.warn('WARNING: invalid number of processes: %i'%np)
+      np = np / ppn
+      time = linesplit[4]
+#      # print times
+#      if lrun: print 'Running: %s'%time  
+#      elif lidl: print 'Idle: %s'%time
+      # convert time format...
+      tmp = time.split(':')
+      if len(tmp) == 3: 
+        time = int(tmp[0])*3600 + int(tmp[1])*60 + int(tmp[2]) 
+      elif len(tmp) == 4:
+        time = int(tmp[0])*86400 + int(tmp[1])*3600 + int(tmp[2])*60 + int(tmp[3])
+      else:
+        warnings.warn('WARNING: invalid time format encountered: %s'%time)
+      # save timings
+      while np > 0:  
+        if lrun: running.append(time)  
+        elif lidl: idle.append(time)
+        np = np - 1
   
   ## estimate total wait time
-  pass
+  # one time slot for each running process
+  slots = numpy.zeros(nodes,dtype=int) # integer seconds
+  # distribute running jobs to nodes
+  assert len(running) <= len(slots), warnings.warn('WARNING: number of nodes and number of runnign jobs inconsistent: %s'%len(running)) 
+  for i in xrange(len(running)):
+    slots[i] = running[i]
+#  print slots
+  # distribute idle jobs to nodes
+  for i in xrange(len(idle)):          
+    slots[slots.argmin()] += idle[i]
+#  print slots
+  
+  ## return estimated wait time
+  waittime = slots.min()
+#  print waittime
+  print('%i'%waittime)
+  
+  
