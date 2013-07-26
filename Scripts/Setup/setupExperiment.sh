e360cd8dcf9e7c21de0cf1c3318f23b901fc5997
Andre R. Erler <aerler@atmosp.physics.utoronto.ca>
2013-07-25 20:59:57 -0400

    * changed default P7 exe to O3NC4  * made some corrections to namelists, based on recent results

--- Scripts/Setup/setupExperiment.sh
+++ Scripts/Setup/setupExperiment.sh
@@ -212,7 +212,7 @@ elif [[ "${WRFSYS}" == "P7" ]]; then
     WRFQ='ll' # queue system
     WRFWCT=${WRFWCT:-'15:00:00'}; WRFNODES=${WRFNODES:-1} # WRF resource config on P7
     GEOEXE=${GEOEXE:-"${WPSSRC}/GPC-MPI/${WPSBLD}/O3xSSSE3/geogrid.exe"}
-    WRFEXE=${WRFEXE:-"${WRFSRC}/P7-MPI/${WRFBLD}/O3pwr7NC4/wrf.exe"}
+    WRFEXE=${WRFEXE:-"${WRFSRC}/P7-MPI/${WRFBLD}/O3NC4/wrf.exe"}
 elif [[ "${WRFSYS}" == "Rocks" ]]; then
     WRFQ='sge' # queue system
     WRFWCT=${WRFWCT:-'4:00:00'}; WRFNODES=${WRFNODES:-1} # WRF resource config on Rocks
