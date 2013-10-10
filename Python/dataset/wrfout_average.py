* finally fixed masking of plev variables (missing values)  * also added missing_value attribute to mean variables

Python/dataset/wrfout_average.py
@@ -117,7 +117,7 @@ if len(period) >= 3:
 liniout = True # indicates that the initialization/restart timestep is written to wrfout;
 # this means that the last timestep of the previous file is the same as the first of the next 
 # input files and folders
-#filetypes = ['hydro'] # for testing 
+#filetypes = ['plev3d'] # for testing 
 filetypes = ['srfc', 'plev3d', 'xtrm', 'hydro']
 inputpattern = 'wrf%s_d%02i_%s-%s-%s_\d\d:\d\d:\d\d.nc' # expanded with %(type,domain,year,month) 
 outputpattern = 'wrf%s_d%02i_monthly.nc' # expanded with %(type,domain)
@@ -171,9 +171,12 @@ def processFileList(pid, filelist, filetype, ndom):
   for devar in derived_vars: devarstr += '%s, '%devar.name
       
   # print meta info (print everything in one chunk, so output from different processes does not get mangled)
-  print('\n\n%s    ***   Processing wrf%s files for domain %2i.   ***'%(pidstr,filetype,ndom) +
-        '\n          (monthly means from %s to %s, incl.)'%(begindate,enddate) +
-        '\n Variable list: %s\n Derived variables: %s'%(varstr,devarstr))
+  titlestr = '\n\n%s    ***   Processing wrf%s files for domain %2i.   ***'%(pidstr,filetype,ndom)
+  titlestr += '\n          (monthly means from %s to %s, incl.)'%(begindate,enddate)
+  if varstr: titlestr += '\n Variable list: %s'%(varstr,)
+  else: titlestr += '\n Variable list: None'
+  if devarstr: titlestr += '\n Derived variables: %s'%(devarstr,)
+  print(titlestr)
   
   # open/create monthly mean output file
   filename = outputpattern%(filetype,ndom)   
@@ -351,7 +354,8 @@ def processFileList(pid, filelist, filetype, ndom):
             tmp = var.__getitem__(slices) # get array
             if missing_value is not None:
               tmp = ma.masked_equal(tmp, missing_value, copy=False) # mask missing values
-            data[varname] += tmp.sum(axis=tax) # add to sum
+            data[varname] = data[varname] + tmp.sum(axis=tax) # add to sum
+	    # N.B.: in-place operations with non-masked array destroy the mask, hence need to use this
         # increment counters
         ntime += wrfendidx - wrfstartidx
         if lcomplete: 
@@ -403,6 +407,9 @@ def processFileList(pid, filelist, filetype, ndom):
         else: data[varname] /= ntime
         # save variable
         var = mean.variables[varname] # this time the destination variable
+	if missing_value is not None: # make sure the missing value flag is preserved
+	  data[varname] = data[varname].filled(fill_value=missing_value)
+	  var.missing_value = missing_value # just to make sure
         if var.ndim > 1: var[meanidx,:] = data[varname] # here time is always the outermost index
         else: var[meanidx] = data[varname]
       # compute derived variables