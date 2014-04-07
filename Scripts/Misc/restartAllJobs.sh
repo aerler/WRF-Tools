53df94e424832259cce4b8312970201f049871ed
Andre R. Erler <aerler@atmosp.physics.utoronto.ca>
2014-04-07 14:34:04 -0400

    * added some TCS status command scripts  * more cleaning

--- Scripts/Misc/restartAllJobs.sh
+++ Scripts/Misc/restartAllJobs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Andre R. Erler, 23/11/2013
+# Andre R. Erler, GPLv3, 23/11/2013
 # script to restart crashed experiments on SciNet
 
 # load list of active experiment (separated by machine)
@@ -21,7 +21,7 @@ function CHECK {
           # figure out next step
           CURRENTSTEP=$( ls [0-9][0-9][0-9][0-9]-[0-9][0-9]* -d | head -n 1 ) # first step folder
           NEXTSTEP=$( ls [0-9][0-9][0-9][0-9]-[0-9][0-9]* -d | tail -n 1 ) # second/last step folder
-          if [[ -f "${INIDIR}/${NEXTSTEP}/run_cycling_WPS"* ]]; then NOWPS='NOWPS' 
+          if [[ -f "${INIDIR}/${NEXTSTEP}/run_cycling_WPS.pbs" ]]; then NOWPS='NOWPS' 
           else NOWPS='FALSE'; fi          
           echo "   NEXTSTEP=${CURRENTSTEP}; NOWPS=${NOWPS}"
           # clean up a bit
@@ -62,15 +62,26 @@ CHECK "${TCS_JOBS}" "${TCS_LIST}" 'TCS'
 P7_LIST=$( ssh p701 'llq -m | grep '\''Job Name'\''' )
 CHECK "${P7_JOBS}" "${P7_LIST}" 'P7'
 
+# count number of jobs
+N=$( echo $GPC_JOBS $TCS_JOBS $P7_JOBS | wc -w )
+# number of jobs unaccounted for
+ERR=$(( $N - $OK - $MIA ))
+
 # report summary
 echo 
-if [ ${MIA} == 0 ]
-  then
+if [ ${OK} == ${N} ]; then
     echo "   <<<   All ${OK} jobs were still running!  >>>   "
-elif [ ${OK} == 0 ]
-  then
-    echo "   ===   ${MIA} jobs were restarted!  ===   "
+elif [ ${MIA} == 0 ]; then
+    echo "   ===   ${OK} jobs were still running. ${ERR} errors encountered!   ===   "
+elif [ ${OK} == 0 ] && [ ${ERR} == 0 ]; then
+    echo "   <<<   ${MIA} jobs were restarted!  >>>   "
+elif [ ${OK} == 0 ]; then
+    echo "   ===   ${MIA} jobs were restarted. ${ERR} errors encountered!   ===   "
+elif [ ${ERR} == 0 ]; then
+    echo "   <<<   ${MIA} jobs were restarted; ${OK} were still running!   >>>   "
 else
-    echo "   ===   ${MIA} jobs were restarted, ${OK} were still running...  ===   "
+    echo "   ===   ${MIA} jobs were restarted; ${OK} were still running. ${ERR} errors encountered!   ===   "
 fi # summary
 echo
+
+exit $ERR
