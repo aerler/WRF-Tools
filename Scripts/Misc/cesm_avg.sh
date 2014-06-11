e49ddd2596eb9980eb5d9d1cab036797416b5486
Andre R. Erler <aerler@atmosp.physics.utoronto.ca>
2014-06-11 18:45:24 -0400

    * updated/fixed cesm averaging script

--- Scripts/Misc/cesm_avg.sh
+++ Scripts/Misc/cesm_avg.sh
@@ -1,70 +1,113 @@
 #!/bin/bash
 # a short script to run averaging operations over a number of CESM archives
 
-#SRCR='/reserved1/p/peltier/marcdo/FromHpss' # Marc's folder on reserved
+#SRCR='/reserved1/p/peltier/marcdo/FromHpss' # Marc's folder on reserved (default)
 #SRCR='/scratch/p/peltier/marcdo/archive/' # Marc's folder on scratch
-SRCR="$CCA" # my archive
-DSTR="$CCA" # my CESM archive folder
+SRCR="$CCA" # my CESM archive folder as source
+DSTR="$CCA" # my CESM archive folder as destination (always)
 
-# historical runs
 shopt -s extglob
-#RUNS='@(h[abc]|t)b20trcn1x1/' # glob expression that identifies CESM archives
-#PERIODS='1979-1988' # averaging period; period defined in avgWRF.py
-# projections
-#RUNS='[abcz]brcp85cn1x1' # glob expression that identifies CESM archives
-RUNS='seaice-5r-hf' # glob expression that identifies CESM archives
-PERIODS='2045-2048' # averaging period; period defined in avgWRF.py
-RECALC='RECALC'
+# for tests
+RUNS='seaice-3r-hf/'
+PERIODS='5' # averaging periods
+# historical runs and projections
+#RUNS='@(h[abc]|t)b20trcn1x1/ h[abct]brcp85cn1x1/ h[abc]brcp85cn1x1d/ seaice-*-hf/'
+#PERIODS='5 10 15' # averaging periods
+# cesm_average settings
+RECALC='FALSE'
+FILETYPES='atm lnd ice'
+
+# feedback
+echo ''
+echo 'Averaging CESM experiments:'
+echo ''
+ls -d $RUNS
+echo ''
+echo "Averaging Periods: ${PERIODS}"
+echo "File Types: ${FILETYPES}"
+echo "Overwriting Files: ${RECALC}"
 
 # loop over runs
 ERRCNT=0
-for AR in $SRCR/$RUNS
+cd "$DSTR"
+for RUN in $RUNS
   do
     echo
     # set up folders
-    RUNDIR=${AR%/} # remove trailing slash, if any
-    RUN=${RUNDIR##*/} # extract highest order folder name as run name
+    RUN=${RUN%/} # remove trailing slash, if any
+    RUNDIR="${SRCR}/${RUN}/" # extract highest order folder name as run name
     AVGDIR="${DSTR}/${RUN}/cesmavg" # subfolder for averages
     mkdir -p "${AVGDIR}" # make sure destination folder exists
-    cd "${AVGDIR}"
-    #if [[ "${DSTR}" != "${SRCR}" ]]
-    #  then
-    #    ln -sf "${SRCR}/${RUN}/atm"  # create a link to source archive
-    #fi # if $DSTR != $SRCR
-    ln -sf "${DSTR}/cesm_average.py" # link archiving script
+    cd "${RUNDIR}"
+    ln -sf "${MODEL_ROOT}/WRF Tools/Python/average/cesm_average.py" # link archiving script
+    # determine period from name
+    if [[ "$RUN" == *20tr* ]]; then START='1979'
+    elif [[ "$RUN" == *rcp*d ]]; then START='2085'
+    elif [[ "$RUN" == *rcp* ]]; then START='2045'
+    elif [[ "$RUN" == seaice-5r-hf ]]; then START='2045 2085'
+    elif [[ "$RUN" == seaice-*-hf ]]; then START='2045'
+    fi # if it doesn't match anything, it will be skipped
+    # calculate periods
+    PRDS='' # clear variable
+    for S in $START; do
+      for PRD in $PERIODS; do
+        PRDS="${PRDS} ${S}-$(( $S + $PRD ))"
+    done; done # loop over start dates and periods
     # loop over averaging periods
-    echo $PERIODS
-    for PERIOD in $PERIODS
+    #echo $PERIODS
+		for FILETYPE in $FILETYPES
       do
-	## start averaging
-	echo "   ***   Averaging $RUN ($PERIOD)   ***   "
-	echo "   ($RUNDIR)"	
-	# launch python script, save output in log file
-	if [[ ! -e "cesmatm_clim_${PERIOD}.nc" ]] || [[ "$RECALC" == 'RECALC' ]]
-	  then
-	    python cesm_average.py "$PERIOD" "$RUNDIR" > "cesm_average_$PERIOD.log"
-	    ERR=$?
-	  else
-	    echo "WARNING: The file cesmatm_clim_${PERIOD}.nc already exits! Skipping computation!"
-	    ERR=0 # count as success
-	fi # if already file exits
-	# clean up
-	if [[ $ERR == 0 ]]
-	  then
-	    echo "   Averaging successful! Saving data to:"
-	    rm 'avgCESM.py' # remove archiving script
-	  else
-	    echo "   WARNING: averaging failed!!! Exit code: $ERR"
-	    ERRCNT=$(( ERRCNT + 1 )) # increase error counter
-	fi # if $ERR
-	echo "   $AVGDIR"
-	echo
+		    ## assemble time-series
+		    case $FILETYPE in
+		      atm) FILES="${RUNDIR}/${FILETYPE}/hist/${RUN}.cam2.h0.";; 
+		      lnd) FILES="${RUNDIR}/${FILETYPE}/hist/${RUN}.clm2.h0.";; 
+		      ice) FILES="${RUNDIR}/${FILETYPE}/hist/${RUN}.cice.h.";; 
+		    esac
+		    # NCO command
+		    NCOARGS="--netcdf4 --deflate 1" # use NetCDF4 compression
+        NCOOUT="${AVGDIR}/cesm${FILETYPE}_monthly.nc"
+		    if [[ ! -e "${NCOOUT}" ]] || [[ "$RECALC" == 'RECALC' ]]; then
+          ncrcat $NCOARGS --output "${NCOOUT}" --overwrite "${FILES}"* > "${NCOOUT%.nc}.log"
+		      ERR=$?		      
+		    else
+		      echo "   Skipping: The file ${NCOOUT} already exits!"
+		      ERR=0 # count as success
+		    fi # if already file exits
+		    if [[ $ERR != 0 ]]; then
+		      echo "   WARNING: CESM Output concatenation failed!!! Exit code: $ERR"
+		      ERRCNT=$(( ERRCNT + 1 )) # increase error counter
+		    fi # if $ERR
+        for PERIOD in $PRDS
+          do
+            ## compute averaged climatologies
+            echo "   ***   Averaging $RUN ($PERIOD,$FILETYPE)   ***   "
+            echo "   ($RUNDIR)"	
+            export PYAVG_FILETYPE=$FILETYPE # set above
+            # launch python script, save output in log file
+            PYAVGOUT="${AVGDIR}/cesm${FILETYPE}_clim_${PERIOD}.nc"
+            if [[ ! -e "$PYAVGOUT" ]] || [[ "$RECALC" == 'RECALC' ]]; then
+              python -u cesm_average.py "$PERIOD" > "${PYAVGOUT%.nc}.log"
+              ERR=$?
+            else
+              echo "   Skipping: The file ${PYAVGOUT} already exits!"
+              ERR=0 # count as success
+            fi # if already file exits
+            # clean up
+            if [[ $ERR == 0 ]]; then
+              echo "   Averaging successful!!!"
+            else
+              echo "   WARNING: Averaging failed!!! Exit code: $ERR"
+              ERRCNT=$(( ERRCNT + 1 )) # increase error counter
+            fi # if $ERR
+            echo
+        done # for FILETYPES
     done # for $PERIODS
+    rm cesm_average.py # remove link to averaging script
 done # for $RUNS
 
 # summary / feedback
 echo
-if [[ $ERR == 0 ]]
+if [[ $ERRCNT == 0 ]]
   then
     echo "   All Operations Successful!!!   "
   else
