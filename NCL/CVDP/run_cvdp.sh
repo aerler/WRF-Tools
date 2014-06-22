6134d491c6449aad8ab082ca82d34c964964591c
Andre R. Erler <aerler@atmosp.physics.utoronto.ca>
2014-06-21 20:08:19 -0400

    * many bugfixes to CESM post-processing driver script (GPL script)

--- NCL/CVDP/run_cvdp.sh
+++ NCL/CVDP/run_cvdp.sh
@@ -23,26 +23,40 @@ OBSDIR="$ROOTDIR/input_obs/" # folder with observational data
 OBSSRC='/reserved1/p/peltier/aerler/CESM/CVDP/obs_input/' # source of obs data
 CVDPSRC="$MODEL_ROOT/WRF Tools/NCL/CVDP/"
 
+# announcement
+echo
+echo "   ***   $RUN ($SPRD-$EPRD)   ***   "
+echo
+echo "    Source Folder: $SRC"
+echo "    Work Directory: $WORKDIR"
+echo
+
 # setup folders
-if [ ! -e "$SRC" ]; then echo "Source folder '$SRC' not found!"; exit 1; fi
-mkdir -p "$DST"
-mkdir -p "$WORKDIR"
-if [ ! -e "$OBSDIR" ]; then cp -r "$OBSSRC" "$OBSDIR"; fi
+if [ ! -e "$SRC" ]; then echo "ERROR: Source folder '$SRC' not found!"; exit 1; fi
+mkdir -p "$DST" "$WORKDIR" "$DATADIR"
+if [ ! -e "$OBSDIR" ]; then echo "   Copying observational data from $OBSSRC to $OBSDIR"; cp -r "$OBSSRC" "$OBSDIR"
+else echo "   Observational data already present: $OBSDIR" ; mkdir -p "$OBSDIR"; fi
+if [[ "$TEST" == 'True' ]]; then ls "$OBSDIR"; echo ''; fi # check that data is present
 # copy CVDP files
 cp "$CVDPSRC/driver.ncl" "$WORKDIR"
 cp -r "$CVDPSRC/ncl_scripts/" "$WORKDIR"
 
 # setup configuration
 cp "$CVDPSRC/namelist_obs" "$WORKDIR"
-sed -i "/obs_input/ s+$SRC+$DST+" "$WORKDIR/namelist_obs" # change obs data source
+sed -i "/obs_input/ s+$OBSSRC+$OBSDIR+" "$WORKDIR/namelist_obs" # change obs data source
 echo " $RUN | $DATADIR | $SPRD | $EPRD " > "$WORKDIR/namelist"
 
 # extract necessary variables
+echo -n "   Copying input data:"
 for V in TS PSL TREFHT PRECT; do # loop over atmospheric variables
+  echo -n " $V,"
   ncks -v $V "$SRC/cesmatm_monthly.nc" "$DATADIR/${V}_${SPRD}01-${EPRD}12.nc"
 done
 # snow is from land module
+V=SNOWDP # last variable (snow depth) is in land file
+echo " $V"
 ncks -v $V "$SRC/cesmlnd_monthly.nc" "$DATADIR/${V}_${SPRD}01-${EPRD}12.nc"
+if [[ "$TEST" == 'True' ]]; then ls "$DATADIR"; echo ''; fi # check that data is present
 
 ## run CVDP
 # some influential environment variables
@@ -55,9 +69,9 @@ ncl driver.ncl
 ERR=$?
 
 # copy results, clean up and exit
-cp "$OUTDIR/"* "$DST"
 echo
 if [ $ERR -ne 0 ]; then 
+  cp "$OUTDIR/"* "$DST"
   echo "ERROR: the NCL driver script exited with exit code $ERR - aborting!"
   echo "       (copying working directory to: '$DMP')"
   cp -r "$WORKDIR/" "$DMP" # copy entire directory to disk
@@ -66,6 +80,7 @@ elif [[ "$TEST" == 'True' ]]; then
   echo "   (copying working directory to: '$DMP')"
   cp -r "$WORKDIR/" "$DMP" # copy entire directory to disk
 else
+  cp "$OUTDIR/"* "$DST"
   echo "   The NCL driver script completed successfully!"
   echo "   (removing working directory: '$WORKDIR')"
 fi   
