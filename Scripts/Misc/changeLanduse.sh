6fc9a459247cf79c996068ee1c5d17fc96b68751
Andre R. Erler <aerler@atmosp.physics.utoronto.ca>
2015-02-09 17:03:34 -0500

    some fixes to script that changes landuse

--- Scripts/Misc/changeLanduse.sh
+++ Scripts/Misc/changeLanduse.sh
@@ -4,12 +4,16 @@
 
 set -e # exit at first sign of trouble
 
+# run geogrid
+mpirun -n 4 ./bin/geogrid.exe
+
+# change landuse in output files
 for GEO in geo_em.d0?.nc
   do
     # convert to classic NetCDF4 format (otherise there are problems)
-    ncks -7 "$GEO" tmp.nc
+    ncks -6 "$GEO" tmp.nc
     # change landuse
-    echo 'yes' | ./bin/read_wrf_nc -EditData LU_INDEX tmp.nc
+    echo 'yes' | ./read_wrf_nc -EditData LU_INDEX tmp.nc
     # save new file
     mv "$GEO" "${GEO%.nc}.orig.nc"
     mv tmp.nc "$GEO"
