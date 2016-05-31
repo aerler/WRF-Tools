bbbe39842583a09bf3bf2bfd46c668f08c766811
Andre R. Erler <A.R.Erler@gmail.com>
2016-05-30 21:42:34 -0400

    updated/fixed lconfig (added src foldrs)

--- Scripts/SyncPP/lconfig.sh
+++ Scripts/SyncPP/lconfig.sh
@@ -12,7 +12,7 @@ NICENESS=${NICENESS:-10}
 # Environment variables used by rsync scripts are defined here
 
 # observational datasets
-DATASETS='Unity GPCC NARR CFSR CRU PRISM PCIC EC WSC' # list of observational datasets
+export DATASETS='Unity GPCC NARR CFSR CRU PRISM PCIC EC WSC' # list of observational datasets
 # WRF & CESM data directories
 export WRFDATA="${DATA_ROOT}/WRF/" # local WRF data root
 export SUBDIR='GreatLakes' # project subfolders
@@ -26,12 +26,12 @@ export CESMDATA="${DATA_ROOT}/CESM/" # local CESM data root
 # special identity/ssh key, batch mode, and connection sharing
 CLUSTER='' # user login for HPC cluster
 CLUSTERSSH="-i ${HOME}/.ssh/rsync -o BatchMode=yes -o ControlPath=${HOME}/master-%l-%r@%h:%p -o ControlMaster=auto -o ControlPersist=1"
-WRFCLSTR='/reserved1/p/peltier/aerler/'
-CESMCLSTR='/reserved1/p/peltier/aerler/CESM/archive/'
-OBSCLSTR='/reserved1/p/peltier/aerler/Datasets/'
+WRFCLSTR='' # WRF root folder on HPC cluster
+CESMCLSTR='' # CESM root folder on HPC cluster
+OBSCLSTR='' # root folder of observational datasets on HPC cluster
 # SSH settings for workstation
 WORKSTN='' # user login for workstation
 WORKSTNSSH="-o BatchMode=yes"
-WRFWRKSTN='/data/WRF/wrfavg/'
-CESMWRKSTN='/data/CESM/cesmavg/'
+WRFWRKSTN='' # WRF root folder on workstation
+CESMWRKSTN='' # CESM root folder on workstation
 # N.B.: the logins can also be SSH configurations which are defined in ${HOME}/.ssh/config
