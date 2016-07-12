bfff0fa5a8ee377f38516e858ff334e433aa5ccb
Andre R. Erler <A.R.Erler@gmail.com>
2016-07-12 14:38:57 -0400

    revised selectWPSqueue and GPC setup  * machine specific config is read from environment  * changed WPS call for GPC to set proper environment

--- Machines/GPC/setup_GPC.sh
+++ Machines/GPC/setup_GPC.sh
@@ -92,7 +92,16 @@ export RUNGEO=${RUNGEO:-"ssh gpc-f104n084-ib0 \"cd ${INIDIR}; source ${SCRIPTDIR
 # export SUBMITWPS=${SUBMITWPS:-'ssh gpc01 "cd \"${INIDIR}\"; qsub ./${WPSSCRIPT} -v NEXTSTEP=${NEXTSTEP}"'} # evaluated by launchPreP
 #export SUBMITWPS=${SUBMITWPS:-'bash -c "cd \"${INIDIR}\"; export WRFWCT=${WRFWCT}; export WPSWCT=${WPSWCT}; export NEXTSTEP=${NEXTSTEP}; export WPSSCRIPT=${WPSSCRIPT}; python ${SCRIPTDIR}/selectWPSqueue.py"'} # use Python script to estimate queue time and choose queue
 # N.B.: the 'bash -c' command is necessary in order to remain consistent with the ssh commands used from other machines
-export SUBMITWPS=${SUBMITWPS:-'ssh gpc-f104n084-ib0 "cd \"${INIDIR}\"; export WRFWCT=${WRFWCT}; export WPSWCT=${WPSWCT}; export NEXTSTEP=${NEXTSTEP}; export WPSSCRIPT=${WPSSCRIPT}; python ${SCRIPTDIR}/selectWPSqueue.py"'} # use Python script to estimate queue time and choose queue
+export SUBMITWPS=${SUBMITWPS:-"$( cat <<-_______END_OF_COMMAND
+      ssh gpc-f104n084-ib0 "cd '${INIDIR}' 
+														export WRFWCT='${WRFWCT}'; export WPSWCT='${WPSWCT}'; export NEXTSTEP='${NEXTSTEP}'
+														export QNDS=4; export QPPN='16,20'; export QSHOW='showq -w class=largemem'
+														export QONE='qsub ${WPSSCRIPT} -v NEXTSTEP={:s} -l nodes=1 -q largemem '
+														export QTWO='qsub ${WPSSCRIPT} -v NEXTSTEP={:s} -l nodes=1:m32g:ppn=8 -q batch'
+														python '${SCRIPTDIR}/selectWPSqueue.py'"
+_______END_OF_COMMAND
+                                 )"} # use Python script to estimate queue time and choose queue
+# N.B.: this is a "here document"; variable substitution should happen at the eval stage
 export WAITFORWPS=${WAITFORWPS:-'NO'} # stay on compute node until WPS for next step finished, in order to submit next WRF job
 
 # archive submission command (for last step in the interval)
