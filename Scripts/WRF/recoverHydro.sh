0059a17091556b544271e923d58b17da8f070a16
Andre R. Erler <A.R.Erler@gmail.com>
2016-10-24 18:21:43 -0400

    fixed many bugs in recoverHydro scripts; converted others to unix format

--- Scripts/WRF/recoverHydro.sh
+++ Scripts/WRF/recoverHydro.sh
@@ -13,32 +13,38 @@ NCSFX=${NCSFX:-'.nc'}
 REGEX=${@:-"wrfout/wrflsm_*$NCSFX"} # default: use LSM files as template
 SRCDIR=${SRCDIR:-"${PWD}"}
 DSTDIR=${DSTDIR:-"${PWD}"}
-SRFCDIM=${SRFCDIM:-'Time,,,4'} # dimension/hyperslab argument (for surface files, which are 6-hourly)
+DIM4=${DIM4:-'Time,,,4'} # dimension/hyperslab argument (for surface files, which are 6-hourly)
 OVERWRITE=${OVERWRITE:-'FALSE'} # skip existing files or overwrite
 
 # begin execution
 echo
 cd "${SRCDIR}"
-ERR=0
+#echo $PWD
+ECC=0
 # loop over files
 for SRC in ${REGEX}
   do
+
+    echo
     #echo $SRC
     # extract domain and time stamp extension
-	   EXT="$( echo "$SRC" | sed -n '/wrf/ s/.*wrf[a-z0-9]*_\(d[0-9]\{2\}_[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}_00[:_]00[:_]00\)${NCSFX}/\1/p' )"
+	  EXT="$( echo "$SRC" | sed -n "/wrf/ s/.*wrf[a-z0-9]*_\(d[0-9]\{2\}_[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}_00[:_]00[:_]00\)${NCSFX}/\1/p" )"
+    #echo $EXT
     # construct destination file name
-	   HYDRO="wrfhydro_${EXT}" # name of hydro file
+	  HYDRO="wrfhydro_${EXT}${NCSFX}" # name of hydro file
+    # clean is in overwrite mode
+    if [[ "${OVERWRITE}" == 'OVERWRITE' ]] && [[ -e "${HYDRO}" ]]; then rm  "${HYDRO}"; fi
     # skip if variable already present (and not OVERWRITE)
-  	if [[ "${OVERWRITE}" == 'OVERWRITE' ]] || [[ ! -e "${HYDRO}" ]]
+  	if [[ ! -e "${HYDRO}" ]]
   	  then
       
         # check prerequisits
         NC=0
-        LSM="wrflsm_${EXT}" # name of LSM file
+        LSM="wrflsm_${EXT}${NCSFX}" # name of LSM file
         [[ ! -e "$LSM" ]] && echo "Input file '$LSM' not found!" && NC=$(( $NC + 1 ))
-        SRFC="wrfxtrm_${EXT}" # name of Surface file
+        SRFC="wrfsrfc_${EXT}${NCSFX}" # name of Surface file
         [[ ! -e "$SRFC" ]] && echo "Input file '$SRFC' not found!" && NC=$(( $NC + 1 ))
-        XTRM="wrfxtrm_${EXT}" # name of Extremes file
+        XTRM="wrfxtrm_${EXT}${NCSFX}" # name of Extremes file
         [[ ! -e "$XTRM" ]] && echo "Input file '$XTRM' not found!" && NC=$(( $NC + 1 ))
            
         
@@ -48,35 +54,40 @@ for SRC in ${REGEX}
             EC=0
             # copy relevant variables from LSM file
             LSMVARS='SFCEVP,ACSNOM,ACSNOW,NOAHRES,POTEVP,Times'
-            ncks -aA -v "$LSMVARS"  "${LSM}" "${DSTDIR}/${HYDRO}" # same time spacing
+            ncks -aA4 -L 1 -v "$LSMVARS"  "${LSM}" "${DSTDIR}/${HYDRO}" # same time spacing
             EC=$(( $EC + $? ))
     		    # copy relevant variables from SRFC file
     		    SRFCVARS='RAINC,RAINNC,I_RAINC,I_RAINNC,SNOWNC,GRAUPELNC,SR'
-            ncks -aA -d "${DIM}" -v "${SRFCVARS}" "${SRFC}" "${DSTDIR}/${HYDRO}" # different time spacing
+            ncks -aA4 -L 1 -d "${DIM4}" -v "${SRFCVARS}" "${SRFC}" "${DSTDIR}/${HYDRO}" # different time spacing
             EC=$(( $EC + $? ))
             # copy variable from Extreme file
             XTRMVARS='T2MEAN'
-            ncks -aA -v "$XTRMVARS"  "${XTRM}" "${DSTDIR}/${HYDRO}" # same time spacing
+            ncks -aA4 -L 1 -v "$XTRMVARS"  "${XTRM}" "${DSTDIR}/${HYDRO}" # same time spacing
             EC=$(( $EC + $? ))                
         else
             EC=1 # missing prerequisites is a failure 
         fi # prerequisites
 
         # check exit code
-				if [[ $? == 0 ]]; then 
-				    echo "Recreated ${HYDRO} successfully"
+				if [ $EC -eq 0 ]; then 
+				    echo "Recreated ${HYDRO} successfully!"
 				else
-				    echo "ERROR: failed to recreate ${HYDRO}"
+				    echo "ERROR: failed to recreate ${HYDRO}!"
 				    ECC=$(( ECC + 1 )) # count error
 		    fi # if error
+        echo
+    else
     
+        echo "Skipping exiting file '$HYDRO'."
+
     fi # if hydro-file not already there
 done # loop over files
 
 echo
-if [[ $ECC == 0 ]]
+if [ $ECC -eq 0 ]
   then
     echo "   ***   Successfully Recreated Hydro-files!   ***   "
   else
     echo "   WARNING: There were ${ECC} Error(s)!"
 fi # $ECC
+echo
