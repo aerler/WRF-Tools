a608c19595e02aa1a9cd25386cf7322988193085
Andre Erler <aerler@atmosp.physics.utoronto.ca>
2018-04-16 22:23:00 -0400

    added login nodes to job submission commands

--- Machines/Niagara/setup_Niagara.sh
+++ Machines/Niagara/setup_Niagara.sh
@@ -102,10 +102,8 @@ export HYBRIDRUN=${HYBRIDRUN:-'mpirun -ppn ${TASKS} -np $((NODES*TASKS))'} # eva
 export RUNGEO=${RUNGEO:-"mpirun -n 4 ${BINDIR}/geogrid.exe"}
 
 # WPS/preprocessing submission command (for next step)
-# export SUBMITWPS=${SUBMITWPS:-'ssh gpc01 "cd \"${INIDIR}\"; qsub ./${WPSSCRIPT} -v NEXTSTEP=${NEXTSTEP}"'} # evaluated by launchPreP
-#export SUBMITWPS=${SUBMITWPS:-'bash -c "cd \"${INIDIR}\"; export WRFWCT=${WRFWCT}; export WPSWCT=${WPSWCT}; export NEXTSTEP=${NEXTSTEP}; export WPSSCRIPT=${WPSSCRIPT}; python ${SCRIPTDIR}/selectWPSqueue.py"'} # use Python script to estimate queue time and choose queue
-# N.B.: the 'bash -c' command is necessary in order to remain consistent with the ssh commands used from other machines
-export SUBMITWPS=${SUBMITWPS:-'cd "${INIDIR}"; sbatch ./${WPSSCRIPT} --export=NEXTSTEP=${NEXTSTEP}'} # no queue selector here
+export SUBMITWPS=${SUBMITWPS:-'ssh nia-login08 "cd \"${INIDIR}\"; qsub ./${WPSSCRIPT} -v NEXTSTEP=${NEXTSTEP}"'} # evaluated by launchPreP
+#export SUBMITWPS=${SUBMITWPS:-'cd "${INIDIR}"; sbatch ./${WPSSCRIPT} --export=NEXTSTEP=${NEXTSTEP}'} # no queue selector here
 # N.B.: this is a "here document"; variable substitution should happen at the eval stage
 export WAITFORWPS=${WAITFORWPS:-'NO'} # stay on compute node until WPS for next step finished, in order to submit next WRF job
 
@@ -115,15 +113,15 @@ export SUBMITAR='' # HPSS is currently not accessible
 # N.B.: requires $ARTAG to be set in the launch script
 
 # averaging submission command (for last step in the interval)
-#export SUBMITAVG=${SUBMITAVG:-'ssh nia-login08 "cd \"${INIDIR}\"; sbatch ./${AVGSCRIPT} --export=PERIOD=${AVGTAG}"'} # evaluated by launchPostP
-export SUBMITAVG=${SUBMITAVG:-"cd \"${INIDIR}\"; sbatch ./${AVGSCRIPT} --export=PERIOD=${AVGTAG}"} # evaluated by launchPostP
+export SUBMITAVG=${SUBMITAVG:-'ssh nia-login08 "cd \"${INIDIR}\"; sbatch ./${AVGSCRIPT} --export=PERIOD=${AVGTAG}"'} # evaluated by launchPostP
+#export SUBMITAVG=${SUBMITAVG:-"cd \"${INIDIR}\"; sbatch ./${AVGSCRIPT} --export=PERIOD=${AVGTAG}"} # evaluated by launchPostP
 # N.B.: requires $AVGTAG to be set in the launch script
 
 # job submission command (for next step)
-#export RESUBJOB=${RESUBJOB-'ssh nia-login08 "cd \"${INIDIR}\"; sbatch ./${WRFSCRIPT} --export=NOWPS=${NOWPS},NEXTSTEP=${NEXTSTEP},RSTCNT=${RSTCNT}"'} # evaluated by resubJob
-export RESUBJOB=${RESUBJOB-"cd \"${INIDIR}\"; sbatch ./${WRFSCRIPT} --export=NOWPS=${NOWPS},NEXTSTEP=${NEXTSTEP},RSTCNT=${RSTCNT}"} # evaluated by resubJob
+export RESUBJOB=${RESUBJOB-'ssh nia-login08 "cd \"${INIDIR}\"; sbatch ./${WRFSCRIPT} --export=NOWPS=${NOWPS},NEXTSTEP=${NEXTSTEP},RSTCNT=${RSTCNT}"'} # evaluated by resubJob
+#export RESUBJOB=${RESUBJOB-"cd \"${INIDIR}\"; sbatch ./${WRFSCRIPT} --export=NOWPS=${NOWPS},NEXTSTEP=${NEXTSTEP},RSTCNT=${RSTCNT}"} # evaluated by resubJob
 
 # sleeper job submission (for next step when WPS is delayed)
-#export SLEEPERJOB=${SLEEPERJOB-'ssh nia-login08 "cd \"${INIDIR}\"; nohup ./${STARTSCRIPT} --restart=${NEXTSTEP} --name=${JOBNAME} &> ${STARTSCRIPT%.sh}_${JOBNAME}_${NEXTSTEP}.log &"'} # evaluated by resubJob; relaunches WPS
-export SLEEPERJOB=${SLEEPERJOB-"cd \"${INIDIR}\"; nohup ./${STARTSCRIPT} --restart=${NEXTSTEP} --name=${JOBNAME} &> ${STARTSCRIPT%.sh}_${JOBNAME}_${NEXTSTEP}.log &"} # evaluated by resubJob; relaunches WPS
+export SLEEPERJOB=${SLEEPERJOB-'ssh nia-login08 "cd \"${INIDIR}\"; nohup ./${STARTSCRIPT} --restart=${NEXTSTEP} --name=${JOBNAME} &> ${STARTSCRIPT%.sh}_${JOBNAME}_${NEXTSTEP}.log &"'} # evaluated by resubJob; relaunches WPS
+#export SLEEPERJOB=${SLEEPERJOB-"cd \"${INIDIR}\"; nohup ./${STARTSCRIPT} --restart=${NEXTSTEP} --name=${JOBNAME} &> ${STARTSCRIPT%.sh}_${JOBNAME}_${NEXTSTEP}.log &"} # evaluated by resubJob; relaunches WPS
 # N.B.: all sleeper jobs should be submitted to P7
