4081e4c6a45549195d9c4c2a4a6615e1d2e71d19
Andre Erler <aerler@atmosp.physics.utoronto.ca>
2020-04-27 22:26:21 -0400

    fixed and test ensemble retrieval script

--- Scripts/Misc/retrieve_ensemble.sh
+++ Scripts/Misc/retrieve_ensemble.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
+ROOT="$PWD"
+
 ## create folders for ensemble members
 # loop over periods
 for P in '' '-2050' '-2100'; do 
-  # generate max-ctrl-* experiments - untested!!!
-  mkdir max-ctrl$P
+  cd "$ROOT/"
+  echo "max-ctrl$P"
+  mkdir -p max-ctrl$P
   cp -P setupExperiment.sh max-ctrl/xconfig.sh max-ctrl$P/    
   cd max-ctrl$P/
   # change name of experiment
   sed -i "/NAME/ s/max-ctrl/max-ctrl$P/" xconfig.sh
-  # N.B.: to actually rerun an experiment, we have to change much more!
+  # change stepfile accoring to period (stepfile is faster than dynamic generation)
+  if [[ "$P" == '-2050' ]]; then
+      sed -i "/CYCLING/ s/monthly.1979-1995/monthly.2045-2060/" xconfig.sh
+  elif [[ "$P" == '-2100' ]]; then
+      sed -i "/CYCLING/ s/monthly.1979-1995/monthly.2085-2100/" xconfig.sh
+  fi # $P
+  # N.B.: To actually rerun an experiment, we msy have to change much more!
+  #       In particular, we need to change the DATADIR for forcing data.
   ./setupExperiment.sh > setupExperiment.log
   # loop over ensemble members
   for E in A B C; do 
-    mkdir max-ens-$E$P
-    cp -P setupExperiment.sh max-ctrl$P/xconfig.sh $E$P    
-    cd $E$P/
-    sed -i "/NAME/ s/max-ctrl$P/max-ens-$E$P/" xconfig.sh
+    cd "$ROOT/"
+    EXP="max-ens-$E$P"
+    echo "$EXP"
+    mkdir -p $EXP
+    cp -P setupExperiment.sh max-ctrl$P/xconfig.sh $EXP/
+    cd $EXP/
+    sed -i "/NAME/ s/max-ctrl$P/$EXP/" xconfig.sh
     ./setupExperiment.sh > setupExperiment.log
   done
 done
@@ -24,7 +37,8 @@ done
 ## retrieve surface fields of an ensemble from HPSS and launch post-processing
 # loop over existing experiment folders
 for E in max-{ctrl,ens-?}{,-2050,-2100}; do 
-    cd $WC/$E 
+    cd "$ROOT/$E"
+    echo "$E"
     # determin period
     if [[ $E = *-2100 ]]; then TAGS="$(seq -s \  2085 2099)";
     elif [[ $E = *-2050 ]]; then TAGS="$(seq -s \  2045 2059)";
